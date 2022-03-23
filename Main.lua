37f8c8b33784ea638387be78a4635f78df1685ce
x3fall3nangel <89339408+x3fall3nangel@users.noreply.github.com>
2022-03-23 18:30:40 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,8 +2,6 @@ if game.PlaceId == 7305309231 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
 elseif game.PlaceId == 7655489843 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
-elseif game.PlaceId == 8917641854 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Sword%20Factory.lua"))()
 elseif game.PlaceId == 7195200675 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
 elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then

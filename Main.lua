b18d9b7879d78a41ddc1bc86678e249fb0e0e385
x3fall3nangel <89339408+x3fall3nangel@users.noreply.github.com>
2022-03-23 18:25:34 +0800

    Add files via upload

---
+++ Main.lua
@@ -0,0 +1,15 @@
+if game.PlaceId == 7305309231 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
+elseif game.PlaceId == 7655489843 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
+elseif game.PlaceId == 8917641854 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Sword%20Factory.lua"))()
+elseif game.PlaceId == 7195200675 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
+elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
+elseif game.PlaceId == 8288069630 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Scripts/main/Stone%20Mining%20sim.lua"))()
+elseif game.PlaceId == 7107498084 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Scripts/main/Anime%20Battle%20Tycoon.lua"))()
+end

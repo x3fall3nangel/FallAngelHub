08ee4ae5f7fa0773b74419ea7ae3b338bd26dabd
GS_StarGamer <75359453+GSstarGamer@users.noreply.github.com>
2022-04-15 16:44:36 +0530

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,6 +6,7 @@ elseif game.PlaceId == 7195200675 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
 elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
-elseif
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Roblox-Exploits/main/loader.lua"))() 
 end
+
+--GS games
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Roblox-Exploits/main/loader.lua"))() 

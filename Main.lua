95542ee13bf5aad99f8f691f402e746fd987fc17
GS_StarGamer <75359453+GSstarGamer@users.noreply.github.com>
2022-04-15 16:42:56 +0530

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,12 +6,6 @@ elseif game.PlaceId == 7195200675 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
 elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
-elseif game.PlaceId == 8288069630 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Scripts/main/Stone%20Mining%20sim.lua"))()
-elseif game.PlaceId == 7107498084 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Scripts/main/Anime%20Battle%20Tycoon.lua"))()
-elseif game.PlaceId == 8781849572 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Scripts/main/Star%20Sim.lua"))()
-elseif game.PlaceId == 4078003854 or game.PlaceId == 4383092793 or game.PlaceId == 2685347741 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Scripts/main/Ghost%20Sim.lua"))()
+elseif
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Roblox-Exploits/main/loader.lua"))() 
 end

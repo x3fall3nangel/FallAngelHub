f69fae30d0c9ccd787f4ffc89e5e64bf0851aa40
GS_StarGamer <75359453+GSstarGamer@users.noreply.github.com>
2022-04-23 08:36:22 +0530

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,12 +1,27 @@
-if game.PlaceId == 7305309231 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
-elseif game.PlaceId == 7655489843 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
-elseif game.PlaceId == 7195200675 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
-elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
+function notif()
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Game Found";
+        Text = "Loading script, wait for 5 sec";
+        })
 end
 
---GS games
+
+if game.PlaceId == 7305309231 then
+    notif()
+    wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
+elseif game.PlaceId == 7655489843 then
+    notif()
+    wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
+elseif game.PlaceId == 7195200675 then
+    notif()
+    wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
+elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
+    notif()
+    wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
+else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Roblox-Exploits/main/loader.lua"))() 
+end

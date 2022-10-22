206e6a22b3d2b36192c36b42683614416ffbd4ac
GS_StarGamer <75359453+GSstarGamer@users.noreply.github.com>
2022-10-22 14:03:23 +0530

    notif
    better notification

--- Main.lua
+++ Main.lua
@@ -1,29 +1,24 @@
-local function notif()
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "Game Found";
-        Text = "Loading script, wait for 3 sec";
-    })
-end
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
 
 
 if game.PlaceId == 7305309231 then
-    notif()
+    Notification.new("success", "Success", "Found script.")
     wait(3)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
 elseif game.PlaceId == 7655489843 then
-    notif()
+    Notification.new("success", "Success", "Found script.")
     wait(3)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
 elseif game.PlaceId == 7195200675 then
-    notif()
+    Notification.new("success", "Success", "Found script.")
     wait(3)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
 elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
-    notif()
+    Notification.new("success", "Success", "Found script.")
     wait(3)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
 elseif game.PlaceId == 9585537847 then
-    notif()
+    Notification.new("success", "Success", "Found script.")
     wait(3)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Shadovis_RPG.lua"))()
 else

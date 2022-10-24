55ae0cb5407744588fb0226383af28cb604381bc
GS_StarGamer <75359453+GSstarGamer@users.noreply.github.com>
2022-10-24 16:25:32 +0530

    better notif

--- Main.lua
+++ Main.lua
@@ -2,25 +2,25 @@ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 
 
 if game.PlaceId == 7305309231 then
-    Notification.new("success", "Success", "Found script.")
-    wait(3)
+    local notif = Notification.new("success", "Found script.", "Loading...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
+    notif:delete()
 elseif game.PlaceId == 7655489843 then
-    Notification.new("success", "Success", "Found script.")
-    wait(3)
+    local notif = Notification.new("success", "Found script.", "Loading...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
+    notif:delete()
 elseif game.PlaceId == 7195200675 then
-    Notification.new("success", "Success", "Found script.")
-    wait(3)
+    local notif = Notification.new("success", "Found script.", "Loading...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
+    notif:delete()
 elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
-    Notification.new("success", "Success", "Found script.")
-    wait(3)
+    local notif = Notification.new("success", "Found script.", "Loading...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
+    local notif = Notification.new("success", "Found script.", "Loading...")
 elseif game.PlaceId == 9585537847 then
-    Notification.new("success", "Success", "Found script.")
-    wait(3)
+    local notif = Notification.new("success", "Found script.", "Loading...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Shadovis_RPG.lua"))()
+    notif:delete()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Roblox-Exploits/main/loader.lua"))() 
 end

542d3b6ec4c3f02dc00e1b2e7cd0d6e2b00f7006
GS_StarGamer <75359453+GSstarGamer@users.noreply.github.com>
2023-06-24 09:44:57 +0530

    anti afk

--- Main.lua
+++ Main.lua
@@ -33,3 +33,10 @@ if status == true then
 elseif status == false then
 
 end
+--anti afk kick
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)

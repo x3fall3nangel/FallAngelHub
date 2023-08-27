8d5db072da0a9a8e28b135e7c30178e80c710adb
x3fall3nangel <lee.jisheng1@gmail.com>
2023-08-27 10:09:21 +0800

    Update MikeCash.lua

--- MikeCash.lua
+++ MikeCash.lua
@@ -1,7 +1,7 @@
 local Title = "Support creator - Please visit the website"
 local API_HOST = "s1.wayauth.com" -- s1.wayauth.com, s2.wayauth.com, s3.wayauth.com, s4.wayauth.com, s5.wayauth.com
 local LINKVERTISE_ID = 939268 -- Change me
-local LINKVERTISE_COUNT = 2 -- Change me
+local LINKVERTISE_COUNT = 1 -- Change me
 local TOKEN_EXPIRE_TIME = 120 -- Seconds
 
 local Task = {}

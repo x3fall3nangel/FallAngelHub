9ce1a0ff016cf9bc3c03ff3f924d0b004b03d8e4
x3fall3nangel <lee.jisheng1@gmail.com>
2024-03-05 20:43:26 +0800

    Update Swordburst3.lua

--- Swordburst3.lua
+++ Swordburst3.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Swordburst 3", HidePremium = false, SaveConfig = true, ConfigFolder = "Swordburst3", ShowIcon = true, Icon = ""})
+local Window = OrionLib:MakeWindow({Name = "Swordburst 3", HidePremium = false, SaveConfig = true, ConfigFolder = "Swordburst3", ShowIcon = true})
 
 local mainTab = Window:MakeTab({
 	Name = "Main",

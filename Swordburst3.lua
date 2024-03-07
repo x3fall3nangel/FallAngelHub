4906812976719119f3a1ea14bf83c188bd2d11a8
x3fall3nangel <lee.jisheng1@gmail.com>
2024-03-07 17:33:32 +0800

    Update Swordburst3.lua

--- Swordburst3.lua
+++ Swordburst3.lua
@@ -1,3 +1,15 @@
+if getgenv().keysystem == true then
+    local KeySystemUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/ui/xrer_mstudio45.lua"))()
+    KeySystemUI.New({
+        ApplicationName = "FallAngelHub", -- Your Key System Application Name
+        Name = "FallAngelHub", -- Your Script name
+        Info = "Get Key For FallAngelHub", -- Info text in the GUI, keep empty for default text.
+        DiscordInvite = "https://discord.gg/auzBFqDrwZ", -- Optional.
+        AuthType = "clientid" -- Can select verifycation with ClientId or IP ("clientid" or "ip")
+    })
+    repeat task.wait() until KeySystemUI.Finished() or KeySystemUI.Closed
+end
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "Swordburst 3", HidePremium = false, SaveConfig = true, ConfigFolder = "Swordburst3", ShowIcon = true,})

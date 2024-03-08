510c2b647e901463df2da8cbc7e20a9fa806d1f0
x3fall3nangel <lee.jisheng1@gmail.com>
2024-03-08 21:06:46 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,16 +2,12 @@ if game.PlaceId == 7305309231 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
 elseif game.PlaceId == 7655489843 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
-elseif game.PlaceId == 7195200675 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Jojo%20The%20Day%20End%20Evolution.lua"))()
-elseif game.PlaceId == 6297822481 or game.PlaceId == 6725802681 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Titan%20Warfare.lua"))()
-elseif game.PlaceId == 9585537847 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Shadovis_RPG.lua"))()
 elseif game.PlaceId == 10704789056 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/DriveWorld.lua"))()
 elseif game.PlaceId == 16582687738 or game.PlaceId == 14819490378 or game.PlaecId == 15695929915 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Swordburst3.lua"))()
+elseif game.PlaceId == 15502802094 or game.PlaceId == 12135640403 or game.PlaceId == 15742087191 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/AnimeDungeonFighters.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GSstarGamer/Roblox-Exploits/main/loader.lua"))()
 end

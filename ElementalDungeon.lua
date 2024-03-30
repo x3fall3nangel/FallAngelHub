ba94d6beb6ac1a129b7da4a3f2e7382fd321503b
x3fall3nangel <lee.jisheng1@gmail.com>
2024-03-30 23:49:32 +0800

    Update ElementalDungeon.lua

--- ElementalDungeon.lua
+++ ElementalDungeon.lua
@@ -1,4 +1,4 @@
-repeat task.wait() until game:GetService("Players").LocalPlayer ~= nil
+repeat task.wait() until game:GetService("Players").LocalPlayer.Character ~= nil
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()

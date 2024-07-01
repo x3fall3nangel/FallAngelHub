a660c470c10cc34b66d99f14f61564d3db050e50
x3fall3nangel <lee.jisheng1@gmail.com>
2024-07-01 13:43:30 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,17 +1,14 @@
 local games = {
-    [1591]
+    [33103002] = "https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/BeyBlade.lua",
+    [4909558] = "https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/DriveWorld.lua",
+    [15914350] = "https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Swordburst3.lua",
+    [15150189] = "https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/AnimeDungeonFighters.lua",
 }
 
-elseif game.PlaceId == 7655489843 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Anime%20Battlegrounds%20X.lua"))()
-elseif game.PlaceId == 10704789056 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/DriveWorld.lua"))()
-elseif game.PlaceId == 16582687738 or game.PlaceId == 14819490378 or game.PlaceId == 15695929915 or game.PlaceId == 16928265611 or game.PlaceId == 17206024999 or game.PlaceId == 17856910664 or game.PlaceId == 18147617256 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Swordburst3.lua"))()
-elseif game.PlaceId == 15502802094 or game.PlaceId == 12135640403 or game.PlaceId == 15742087191 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/AnimeDungeonFighters.lua"))()
-elseif game.PlaceId == 15423741735 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/BeyBlade.lua"))()
+if games[game.CreatorId] then
+    loadstring(game:HttpGet(games[game.CreatorId]))()
+else
+    print("Game not supported")
 end
 
 --anti afk kick
@@ -20,8 +17,4 @@ game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    wait(1)
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
-
-game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(state)
-    queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Main.lua"))()]])
 end)
